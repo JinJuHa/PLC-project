076cd5679503815501913ae77c66c7929f54bf1b
zi12i <110373482+zi12i@users.noreply.github.com>
2022-12-05 16:11:28 +0900

    init

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Auth from './models/auth'
+import User from './models/user'
+
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    Auth,
+    User,
+  }
+})
