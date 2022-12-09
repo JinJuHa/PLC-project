231d539159e371e463aed20272df0c5e7bfdd08b
zi12i <110373482+zi12i@users.noreply.github.com>
2022-12-09 16:20:58 +0900

    doughnut so far

--- src/views/dashboard/rough.vue
+++ src/views/dashboard/rough.vue
@@ -22,7 +22,17 @@
           <p>불량품, 양품 비율</p>
         </div>
       </div>
-      <div class="dashboard-doughnut"></div>
+      <div class="dashboard-doughnut">
+        <!-- <doughnut-chart ref="chart" :chartData="chart.data" :options="options" style="width: 450px height: 250px">
+        </doughnut-chart> -->
+        <doughnut-chart
+          id="chart"
+          ref="fruitChart"
+          :chart-data="chart.data"
+          :options="chart.options"
+          style="width: 450px; height: 250px"
+        ></doughnut-chart>
+      </div>
       <div class="dashboard-bar"></div>
     </div>
     <div class="dashboard-footer"></div>
@@ -30,10 +40,70 @@
 </template>
 
 <script>
+import DoughnutChart from '@/components/chart/doughnutChart'
 export default {
-  data: () => ({
-    담당자이름: '지미'
-  }),
+  components: {
+    'doughnut-chart': DoughnutChart
+  },
+  data() {
+    return {
+      담당자이름: '지미',
+      chart: {
+        data: {
+          labels: ['딸기', '자두', '수박', '복숭아', '메론', '망고'],
+          datasets: [
+            {
+              backgroundColor: ['#A684B7', '#DD7445', '#DE9D11', '#E0D295', '#B1D166', '#78BAA1'],
+              borderColor: '#eee',
+              hoverBorderColor: '#eee',
+              data: [50, 30, 20, 40, 60, 10]
+            }
+          ]
+        },
+        options: {
+          plugins: {
+            legend: {
+              display: true,
+              position: 'left',
+              labels: {
+                boxWidth: 8,
+                padding: 10,
+                usePointStyle: true,
+                pointStyle: 'circle',
+                font: {
+                  size: 14
+                }
+              },
+              fullSize: true,
+              align: 'center'
+            },
+            tooltip: {
+              boxWidth: 15,
+              bodyFont: {
+                size: 14
+              }
+            }
+          },
+          responsive: true,
+          maintainAspectRatio: false,
+          layout: {
+            padding: {
+              top: 50,
+              bottom: 50
+            }
+          },
+          elements: {
+            arc: {
+              borderWidth: 2
+            }
+          },
+          animation: {
+            duration: 5000
+          }
+        }
+      }
+    }
+  },
   mounted() {
     this.timerInterval = setInterval(() => {
       const now = new Date()
@@ -42,7 +112,7 @@ export default {
       let dates = now.getDate()
       this.today = `${years}/${months}/${dates}`
       document.querySelector('#time').innerHTML = now.toLocaleString('ko-kr')
-    }, 10) // 1초마다 함수 실행되도록 설정
+    }, 10)
   },
   destroyed() {
     clearInterval(this.timerInterval)
@@ -55,17 +125,17 @@ export default {
 .dateTime {
   text-align: left;
   width: 100%;
-  color: rgb(255, 255, 255);
-  border-left: 3px solid #ed1f24;
+  color: darkgreen;
+  border-left: 3px solid green;
   padding-left: 20px;
   font-size: 40px;
 }
 .dashboard-container {
   display: grid;
   grid-template-rows: 25% 45% 30%;
-  width: 100%;
-  height: 100vh;
-  /* background-color: green; */
+  width: 90%;
+  height: 90vh;
+  background-color: green;
   padding: 5px;
 }
 .dashboard-header {
@@ -73,37 +143,35 @@ export default {
   grid-template-rows: 30% 70%;
   width: 100%;
   height: 100%;
-  /* background-color: yellow; */
+  color: white;
   padding: 5px;
 }
 .dashboard-headding {
-  background-color: aliceblue;
   letter-spacing: 2px;
-  border-radius: 20px;
   padding: auto;
-  color: grey;
   font-size: 35px;
   width: 100%;
   height: 100%;
   text-align: center;
 }
 .dashboard-info {
-  background-color: rgb(9, 15, 80);
+  background-color: white;
   display: grid;
   grid-template-columns: 70% 30%;
   width: 100%;
   height: 100%;
-  padding: 5px;
+  padding: 20px;
+  border-radius: 10px;
 }
 .dashboard-date {
-  /* background-color: azure; */
   padding: 5px;
   color: grey;
   font-size: 30px;
 }
 .dashboard-user {
-  background-color: aliceblue;
-  padding: 5px;
+  color: darkgreen;
+  font-size: 45px;
+  padding: 20px;
   font-size: 30px;
 }
 .dashboard-columns {
@@ -111,25 +179,28 @@ export default {
   grid-template-columns: 30% 35% 35%;
   width: 100%;
   height: 100%;
-  background-color: orange;
+  background-color: white;
+  border-radius: 10px;
   padding: 5px;
 }
 .dashboard-rates {
   display: grid;
   grid-template-rows: 50% 50%;
   padding: 5px;
-  background-color: bisque;
+  /* background-color: bisque; */
 }
 .dashboard-amount {
   width: 100%;
   height: 100%;
   background-color: rgb(214, 219, 250);
   padding: 5px;
+  text-align: center;
 }
 .dashboard-doughnut {
   width: 100%;
   height: 100%;
-  background-color: brown;
+  border-left: solid 1px;
+  border-right: solid 1px;
 }
 .dashboard-bar {
   width: 100%;
